83f3c6a695c9cdeb259073cce34a0c8b3b33824a
Carter Pry <117807297+CarterPry@users.noreply.github.com>
2023-02-12 20:57:11 -0500

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,78 +1,106 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import random
-
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    game_grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    move = request.form.get("move")
+griddy = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-    if request.method == "POST":
-        if move == "up":
-            game_grid = move_up(game_grid)
-        elif move == "down":
-            game_grid = move_down(game_grid)
-        elif move == "left":
-            game_grid = move_left(game_grid)
-        elif move == "right":
-            game_grid = move_right(game_grid)
-
-        # add a new value to the grid after each move
-        empty_cells = [i for i, x in enumerate(game_grid) if x == 0]
-        if empty_cells:
-            index = random.choice(empty_cells)
-            game_grid[index] = 2
-
-    return render_template("2048.html", game_grid=game_grid)
-
-def move_up(grid):
-    new_grid = [0] * 16
-    for col in range(4):
-        current_col = [grid[row * 4 + col] for row in range(4)]
-        current_col = merge_values(current_col)
-        for row in range(4):
-            new_grid[row * 4 + col] = current_col[row]
-    return new_grid
-
-def move_down(grid):
-    new_grid = [0] * 16
-    for col in range(4):
-        current_col = [grid[row * 4 + col] for row in range(3, -1, -1)]
-        current_col = merge_values(current_col)
-        for row in range(4):
-            new_grid[(3 - row) * 4 + col] = current_col[row]
-    return new_grid
-
-def move_left(grid):
-    new_grid = [0] * 16
+def left(griddy):
     for row in range(4):
-        current_row = grid[row * 4 : row * 4 + 4]
-        current_row = merge_values(current_row)
-        new_grid[row * 4 : row * 4 + 4] = current_row
-    return new_grid
+        empty = [i for i in range(4) if griddy[row][i] == 0]
+        non_empty = [i for i in range(4) if griddy[row][i] != 0]
+        griddy[row] = [griddy[row][i] for i in non_empty] + [0 for i in empty]
+        for i in range(3):
+            if griddy[row][i] == griddy[row][i + 1]:
+                griddy[row][i] *= 2
+                griddy[row][i + 1] = 0
+        empty = [i for i in range(4) if griddy[row][i] == 0]
+        non_empty = [i for i in range(4) if griddy[row][i] != 0]
+        griddy[row] = [griddy[row][i] for i in non_empty] + [0 for i in empty]
+    return griddy
 
-def move_right(grid):
-    new_grid = [0] * 16
+
+
+def right(griddy):
     for row in range(4):
-        current_row = grid[row * 4 : row * 4 + 4]
-        current_row = merge_values(current_row)[::-1]
-        new_grid[row * 4 : row * 4 + 4] = current_row[::-1]
-    return new_grid
+        empty = [i for i in range(4) if griddy[row][3 - i] == 0]
+        non_empty = [i for i in range(4) if griddy[row][3 - i] != 0]
+        griddy[row] = [0 for i in empty] + [griddy[row][3 - i] for i in non_empty]
+        for i in range(3, 0, -1):
+            if griddy[row][i] == griddy[row][i - 1]:
+                griddy[row][i] *= 2
+                griddy[row][i - 1] = 0
+        empty = [i for i in range(4) if griddy[row][3 - i] == 0]
+        non_empty = [i for i in range(4) if griddy[row][3 - i] != 0]
+        griddy[row] = [0 for i in empty] + [griddy[row][3 - i] for i in non_empty]
+    return griddy
 
-def merge_values(values):
-# merge adjacent cells with the same value
-i = 0
-while i < len(values) - 1:
-if values[i] == values[i + 1]:
-values[i] *= 2
-del values[i + 1]
-values.append(0)
-i += 1
-# move all values to the left
-new_values = [x for x in values if x != 0]
-new_values += [0] * (len(values) - len(new_values))
-return new_values
 
-if name == "main":
- app.run(debug=True)
+
+def up(griddy):
+    griddy = [[griddy[j][i] for j in range(4)] for i in range(4)]
+    griddy = left(griddy)
+    griddy = [[griddy[j][i] for j in range(4)] for i in range(4)]
+    return griddy
+
+
+
+def down(griddy):
+    griddy = [[griddy[j][i] for j in range(4)] for i in range(4)]
+    griddy = right(griddy)
+    griddy = [[griddy[j][i] for j in range(4)] for i in range(4)]
+    return griddy
+
+
+
+@app.route('/')
+def index():
+    global griddy
+    griddy = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    pop_up()
+    return render_template('index.html', griddy=griddy)
+
+
+
+@app.route('/move/<direction>')
+def move(direction):
+    global griddy
+    if direction == 'left':
+        pop_up()
+        griddy = left(griddy)
+    elif direction == 'right':
+        pop_up()
+        griddy = right(griddy)
+    elif direction == 'up':
+        pop_up()
+        griddy = up(griddy)
+    elif direction == 'down':
+        pop_up()
+        griddy = down(griddy)
+        pop_up()
+    return render_template('index.html', griddy = griddy)
+
+
+
+@app.route('/random')
+def pop_up():
+    global griddy
+    empty_locations = [(i, j) for i in range(4) for j in range(4) if griddy[i][j] == 0]
+    if len(empty_locations) == 0:
+        return
+    elif len(empty_locations) == 1:
+        i, j = empty_locations[0]
+        griddy[i][j] = 2 if random.random() < 0.9 else 4
+    else:
+        random_location1, random_location2 = random.sample(empty_locations, 2)
+        i1, j1 = random_location1
+        i2, j2 = random_location2
+        griddy[i1][j1] = 2
+        griddy[i2][j2] = 2
+    return render_template('index.html', griddy=griddy)
+
+    
